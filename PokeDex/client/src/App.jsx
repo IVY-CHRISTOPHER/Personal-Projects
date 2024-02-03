d3403a2a0edc148a08ef026295ca248ccf172f3f
Christopher Ivy <CHRISIVY22703@GMAIL.COM>
2024-02-02 21:58:31 -0600

    Chore: Added browser router and rendering the login page on load also linked a register page

--- PokeDex/client/src/App.jsx
+++ PokeDex/client/src/App.jsx
@@ -1,8 +1,17 @@
 import { useState } from "react";
+import { Route, Routes, Navigate } from 'react-router-dom'
+import Login from "../components/Login"
+import Register from "../components/Register"
 import "./App.css";
 
 const App = () => {
-  return <div>Um...Hello!?</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  )
 };
 
 export default App;
